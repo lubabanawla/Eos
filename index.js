127f91e079203a43dda43b3335f3ff3b9345ecca
Lubaba Nawla <lubabanawla@gmail.com>
2025-03-05 01:43:24 -0500

    Add files via upload
    Fixed the karaoke part

--- index.js
+++ index.js
@@ -4,173 +4,160 @@ const songs = [
       id: 1,
       title: "We Are The People",
       artist: "Empire of the Sun",
-      // Approximate duration in seconds (6:15)
-      duration: 375,
+      // Approximate duration in seconds (4:27)
+      duration: 257,
       src: "wearethepeople.mp3",
       cover: "coverwearethepeople.jpeg",
-      lyrics: `[00:00.00]Verse:
-    [00:05.00]We can remember
-    [00:10.00]Swimming in December
-    [00:15.00]Heading for the city lights in 1975
-    [00:20.00]We share in each other
-    [00:25.00]Nearer than farther
-    [00:30.00]The scent of a lemon drips from your eyes
-    [00:35.00]Chorus:
-    [00:40.00]We are the people that rule the world
-    [00:45.00]A force running in every boy and girl
-    [00:50.00]All rejoicing in the world
-    [00:55.00]Take me now, we can try
-    [01:00.00]Verse:
-    [01:05.00]We lived an adventure
-    [01:10.00]Love in the summer
-    [01:15.00]Followed the sun till night
-    [01:20.00]Reminiscing other times of life
-    [01:25.00]For each every other
-    [01:30.00]The feeling was stronger
-    [01:35.00]The shock hit eleven, got lost in your eyes
-    [01:40.00]Chorus:
-    [01:45.00]I can’t do well
-    [01:50.00]When I think you're gonna leave me
-    [01:55.00]But I’m gonna try
-    [02:00.00]Are you gonna leave me now?
-    [02:05.00]Can't you be believing now?
-    [02:10.00]I can't do well
-    [02:15.00]When I think you’re gonna leave me
-    [02:20.00]But I'm gonna try
-    [02:25.00]Are you gonna leave me now?
-    [02:30.00]Can't you be believing now?
-    [02:35.00]Verse:
-    [02:40.00]Can you remember and humanize?
-    [02:45.00]It was still where we’d energised
-    [02:50.00]Lie in the sand and visualise
-    [02:55.00]Like it's '75 again
-    [03:00.00]Chorus:
-    [03:05.00]We are the people that rule the world
-    [03:10.00]A force running in every boy and girl
-    [03:15.00]All rejoicing in the world
-    [03:20.00]Take me now, we can try
-    [03:25.00]I can't do well
-    [03:30.00]When I think you're gonna leave me
-    [03:35.00]But I'm gonna try
-    [03:40.00]Are you gonna leave me now?
-    [03:45.00]Can't you be believing now?
-    [03:50.00]I can’t do well
+      lyrics: `
+    [00:16.00]We can remember
+    [00:20.00]Swimming in December
+    [00:24.00]Heading for the city lights in 1975
+    [00:31.00]We share in each other
+    [00:35.00]Nearer than farther
+    [00:39.00]The scent of a lemon drips from your eyes
+    [00:47.00]We are the people that rule the world
+    [00:51.00]A force running in every boy and girl
+    [00:55.00]All rejoicing in the world
+    [00:58.00]Take me now, we can try
+    [01:02.00]We lived an adventure
+    [01:07.00]Love in the summer
+    [01:10.00]Followed the sun till night
+    [01:13.00]Reminiscing other times of life
+    [01:18.00]For each every other
+    [01:23.00]The feeling was stronger
+    [01:26.00]The shock hit eleven, got lost in your eyes
+    [01:33.00]I can’t do well
+    [01:35.00]When I think you're gonna leave me
+    [01:36.00]But I’m gonna try
+    [01:40.00]Are you gonna leave me now?
+    [01:44.00]Can't you be believing now?
+    [01:48.00]I can't do well
+    [01:50.00]When I think you’re gonna leave me
+    [01:52.00]But I'm gonna try
+    [01:55.00]Are you gonna leave me now?
+    [01:59.00]Can't you be believing now?
+    [02:05.00]Can you remember and humanize?
+    [02:10.00]It was still where we’d energised
+    [02:13.00]Lie in the sand and visualise
+    [02:16.00]Like it's '75 again
+    [02:21.00]We are the people that rule the world
+    [02:25.00]A force running in every boy and girl
+    [02:28.00]All rejoicing in the world
+    [02:32.00]Take me now, we can try
+    [02:35.00]I can't do well
+    [02:37.00]When I think you're gonna leave me
+    [02:39.00]But I'm gonna try
+    [02:42.00]Are you gonna leave me now?
+    [02:47.00]Can't you be believing now?
+    [02:51.00]I can’t do well
+    [02:53.00]When I think you're gonna leave me
+    [02:54.00]But I'm gonna try
+    [02:58.00]Are you gonna leave me now?
+    [03:02.00]Can't you be believing now?
+    [03:07.00]I know everything about you
+    [03:11.00]You know everything about me
+    [03:15.00]We know everything about us
+    [03:22.00]I know everything about you
+    [03:26.00]You know everything about me
+    [03:30.00]We know everything about us
+    [03:38.00]I can't do well
+    [03:39.00]When I think you’re gonna leave me
+    [03:41.00]But I'm gonna try
+    [03:45.00]Are you gonna leave me now?
+    [03:48.00]Can't you be believing now?
+    [03:53.00]I can't do well
     [03:55.00]When I think you're gonna leave me
-    [04:00.00]But I'm gonna try
-    [04:05.00]Are you gonna leave me now?
-    [04:10.00]Can't you be believing now?
-    [04:15.00]Bridge:
-    [04:20.00]I know everything about you
-    [04:25.00]You know everything about me
-    [04:30.00]We know everything about us
-    [04:35.00]I know everything about you
-    [04:40.00]You know everything about me
-    [04:45.00]We know everything about us
-    [04:50.00]Chorus:
-    [04:55.00]I can't do well
-    [05:00.00]When I think you’re gonna leave me
-    [05:05.00]But I'm gonna try
-    [05:10.00]Are you gonna leave me now?
-    [05:15.00]Can't you be believing now?
-    [05:20.00]I can't do well
-    [05:25.00]When I think you're gonna leave me
-    [05:30.00]But I'm gonna try
-    [05:35.00]Are you gonna leave me now?
-    [05:40.00]Can't you be believing now?
-    [05:45.00]I can't do well
-    [05:50.00]When I think you're gonna leave me
-    [05:55.00]But I'm gonna try
-    [06:00.00]Are you gonna leave me now?
-    [06:05.00]Can't you be believing now?
-    [06:10.00]Outro:
-    [06:15.00]I can't do well`
+    [03:57.00]But I'm gonna try
+    [04:00.00]Are you gonna leave me now?
+    [04:04.00]Can't you be believing now?
+    [04:09.00]I can't do well
+    [04:11.00]When I think you're gonna leave me
+    [04:12.00]But I'm gonna try
+    [04:16.00]Are you gonna leave me now?
+    [04:20.00]Can't you be believing now?
+    [04:25.00]I can't do well`
     },
     {
       id: 2,
       title: "The Wanderlust",
       artist: "Metric",
-      // Approximate duration in seconds (5:20)
-      duration: 320,
+      // Approximate duration in seconds (3:10)
+      duration: 186,
       src: "thewanderlust.mp3",
       cover: "coverthewanderlust.jpeg",
-      lyrics: `[00:00.00]Verse:
-    [00:05.00]I’ve been sleepwalking (I've been sleepwalking)
-    [00:10.00]Through the railway station (through the railway station)
-    [00:15.00]Wake me up (wake me up)
-    [00:20.00]When they call my destination (when they call my destination)
-    [00:25.00]Where I’ve been (where I've been)
-    [00:30.00]I was bound to leave behind (I was bound to leave behind)
-    [00:35.00]All device (all device)
-    [00:40.00]And all disease was mine (and all disease was mine)
-    [00:45.00]Pre-Chorus:
-    [00:50.00]Oh, speeding out of reach
-    [00:55.00]Oh, you're the one I had to meet
-    [01:00.00]Chorus:
-    [01:05.00]I never wanted to go home
-    [01:10.00](The wanderlust will carry your song)
-    [01:15.00]There was nothing there for me
-    [01:20.00](The wanderlust will carry your song)
-    [01:25.00]In a high rise on my own
-    [01:30.00](I’ve been on the brinks of highways for so long)
-    [01:35.00]Looking out in a mirrored balcony
-    [01:40.00](The wanderlust will carry your song)
-    [01:45.00]Will there ever be a place for you and me?
-    [01:50.00](The wanderlust)
-    [01:55.00]Verse:
-    [02:00.00]I’m rolling in (I'm rolling in)
-    [02:05.00]For a seven day weekend (seven day weekend)
-    [02:10.00]Living up again (living up again)
-    [02:15.00]To my old reputation (my old reputation)
-    [02:20.00]Can you cover me (can you cover me)
-    [02:25.00]’Cause I got no armour ('cause I got no armour)
-    [02:30.00]Keep moving on down the line
-    [02:35.00]Keep on going further (keep on going further)
-    [02:40.00]Pre-Chorus:
-    [02:45.00]Oh, speeding out of reach
-    [02:50.00]Oh, you’re the one I had to meet
-    [02:55.00]Chorus:
-    [03:00.00]I never wanted to go home
-    [03:05.00](The wanderlust will carry your song)
-    [03:10.00]There was nothing there for me
-    [03:15.00](The wanderlust will carry your song)
-    [03:20.00]In a high rise on my own
-    [03:25.00](I've been on the brinks of highways so long)
-    [03:30.00]Looking out in a mirrored balcony
-    [03:35.00](The wanderlust will carry, carry, carry your song)
-    [03:40.00]I don’t know where to go, now I got nowhere to hide
-    [03:45.00](Nowhere to go, nowhere to be)
-    [03:50.00]I don't know where to go, now I got nowhere to be
-    [03:55.00](Oh, wanderlust will carry your song)
-    [04:00.00]Walk fifty city blocks down but they always find us
-    [04:05.00](The wanderlust will carry your song)
-    [04:10.00]Will there ever be a place for you and me?
-    [04:15.00](The wanderlust will carry your song)
-    [04:20.00]Will there ever be a place for you and me?`
+      lyrics: `
+    [00:00.00]I’ve been sleepwalking (I've been sleepwalking)
+    [00:03.00]Through the railway station (through the railway station)
+    [00:08.00]Wake me up (wake me up)
+    [00:10.00]When they call my destination (when they call my destination)
+    [00:14.00]Where I’ve been (where I've been)
+    [00:18.00]I was bound to leave behind (I was bound to leave behind)
+    [00:22.00]All device (all device)
+    [00:25.00]And all disease was mine (and all disease was mine)
+    [00:29.00]Oh, speeding out of reach
+    [00:36.00]Oh, you're the one I had to meet
+    [00:45.00]I never wanted to go home
+    [00:48.00](The wanderlust will carry your song)
+    [00:52.00]There was nothing there for me
+    [00:56.00](The wanderlust will carry your song)
+    [00:59.00]In a high rise on my own
+    [01:02.00](I’ve been on the brinks of highways for so long)
+    [01:05.00]Looking out in a mirrored balcony
+    [01:09.00](The wanderlust will carry your song)
+    [01:12.00]Will there ever be a place for you and me?
+    [01:17.00](The wanderlust)
+    [01:19.00]I’m rolling in (I'm rolling in)
+    [01:22.00]For a seven day weekend (seven day weekend)
+    [01:26.00]Living up again (living up again)
+    [01:29.00]To my old reputation (my old reputation)
+    [01:33.00]Can you cover me (can you cover me)
+    [01:37.00]’Cause I got no armour ('cause I got no armour)
+    [01:41.00]Keep moving on down the line
+    [01:43.00]Keep on going further (keep on going further)
+    [01:48.00]Oh, speeding out of reach
+    [01:55.00]Oh, you’re the one I had to meet
+    [02:03.00]I never wanted to go home
+    [02:08.00](The wanderlust will carry your song)
+    [02:11.00]There was nothing there for me
+    [02:15.00](The wanderlust will carry your song)
+    [02:18.00]In a high rise on my own
+    [02:21.00](I've been on the brinks of highways so long)
+    [02:24.00]Looking out in a mirrored balcony
+    [02:27.00](The wanderlust will carry, carry, carry your song)
+    [02:31.00]I don’t know where to go, now I got nowhere to hide
+    [02:36.00](Nowhere to go, nowhere to be)
+    [02:38.00]I don't know where to go, now I got nowhere to be
+    [02:42.00](Oh, wanderlust will carry your song)
+    [02:45.00]Walk fifty city blocks down but they always find us
+    [02:49.00](The wanderlust will carry your song)
+    [02:52.00]Will there ever be a place for you and me?
+    [02:56.00](The wanderlust will carry your song)
+    [03:00.00]Will there ever be a place for you and me?`
     },
     {
       id: 3,
       title: "You Are The Right One",
       artist: "Sports",
-      // Approximate duration in seconds (4:00)
-      duration: 240,
+      // Approximate duration in seconds (2:52)
+      duration: 152,
       src: "youaretherightone.mp3",
       cover: "coveryouaretherightone.jpg",
-      lyrics: `[00:00.00]You were the right way
-    [00:05.00]I was just waitin′ for you to look at me
-    [00:10.00]Is there a wrong time?
-    [00:15.00]Baby, I'm guessin′
-    [00:20.00]Just let me know
-    [00:25.00]Just let me know
-    [00:30.00]Well, it's been a long time
-    [00:35.00]Since you've been lonely
-    [00:40.00]So what will I do?
-    [00:45.00]You are the right one
-    [00:50.00]And I′m just the boy who is lookin′ at you
-    [00:55.00]So what will I do?
-    [01:00.00]I tried the wrong way
-    [01:05.00]I was just guessin', bidin′ my time
-    [01:10.00]You are the only one I can picture by my side
-    [01:15.00]By my side`
+      lyrics: `[00:34.00]You were the right way
+    [00:39.00]I was just waitin′ for you to look at me
+    [00:44.00]Is there a wrong time?
+    [00:48.00]Baby, I'm guessin′
+    [00:51.00]Just let me know
+    [01:00.00]Just let me know
+    [01:12.00]Well, it's been a long time
+    [01:17.00]Since you've been lonely
+    [01:20.00]So what will I do?
+    [01:22.00]You are the right one
+    [01:27.00]And I′m just the boy who is lookin′ at you
+    [01:38.00]So what will I do?
+    [01:51.00]I tried the wrong way
+    [01:56.00]I was just guessin', bidin′ my time
+    [02:00.00]You are the only one I can picture by my side
+    [02:17.00]By my side`
     }
   ];
   
